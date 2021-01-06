a0cec702c15fe2889e191502fe9d19e68ab4d1d5
Tal Harouvi <halouvi@gmail.com>
2021-01-06 12:53:48 +0200

    merge 6.1 1pm

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,5 +1,4 @@
 const dbService = require('../../services/db.service')
-const { use } = require('../booking/booking.routes')
 const ObjectId = require('mongodb').ObjectId
 
 module.exports = {
@@ -8,8 +7,6 @@ module.exports = {
   remove,
   update,
   add,
-  unreadBooking,
-  resetUnreadBookings
 }
 
 async function getById(userId) {
@@ -56,35 +53,8 @@ async function update(user) {
   }
 }
 
-async function unreadBooking(user) {
-  const collection = await dbService.getCollection('user')
-  user._id = ObjectId(user._id)
-  try {
-    const res = await collection.updateOne({ _id: user._id }, { $inc: {unreadBookings: 1} });
-    // const updatedUser = await collection.findOne({ _id: user._id });
-    return res
-  } catch (err) {
-    console.log(`ERROR: cannot update unread bookings ${user._id}`)
-    throw err
-  }
-}
-
-async function resetUnreadBookings(user) {
-  const collection = await dbService.getCollection('user')
-  user._id = ObjectId(user._id)
-  try {
-    await collection.updateOne({ _id: user._id }, { $set: {unreadBookings: 0} });
-    const updatedUser = await collection.findOne({ _id: user._id });
-    return updatedUser
-  } catch (err) {
-    console.log(`ERROR: cannot update unread bookings ${user._id}`)
-    throw err
-  }
-}
-
 async function add(user) {
   const collection = await dbService.getCollection('user')
-  // user.unreadBookings = 0;
   try {
     await collection.insertOne(user)
     return user
