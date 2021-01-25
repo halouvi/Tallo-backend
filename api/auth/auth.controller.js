2e8ba680b1820eb67fe7f44e0283b66f56cdc402
Tal Harouvi <halouvi@gmail.com>
2021-01-25 20:37:23 +0200

    token login

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,16 +1,26 @@
-const {authService} = require('./auth.service')
+const { authService } = require('./auth.service')
 const logger = require('../../services/logger.service')
 const { boardService } = require('../board/board.service')
 
 module.exports = {
   login: async (req, res) => {
     try {
-      const user = await authService.login(req.body)
+      const { user, login } =
+        req.body.email || req.body.password
+          ? await authService.login(req.body)
+          : req.cookies.login
+          ? await authService.tokenLogin(req.cookies.login)
+          : {}
+      if (!user || !login) throw new Error('ERROR: No credentials sent')
       req.session.user = user
+      res.cookie('login', login, {
+        maxAge: 259200000, // 3 days
+        httpOnly: true
+      })
       const userBoards = await boardService.getBoardsById(user.boards)
       res.send({ user, userBoards })
-    } catch (err) {
-      res.status(401).send({ error: err })
+    } catch (error) {
+      res.status(401).send({ error })
     }
   },
 
@@ -18,7 +28,6 @@ module.exports = {
     try {
       const { email, password, fullname, imgUrl, boards } = req.body
       logger.debug(email + ', ' + fullname + ', ' + password + ',' + imgUrl + ',' + boards)
-      console.log(req.body)
       const account = await authService.signup(req.body)
       logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
       const user = await authService.login(req.body)
@@ -33,9 +42,14 @@ module.exports = {
   logout: async (req, res) => {
     try {
       req.session.destroy()
+      await authService.logout(req.cookies.login)
+      res.cookie('login', null, {
+        maxAge: 0,
+        httpOnly: true
+      })
       res.send({ message: 'logged out successfully' })
-    } catch (err) {
-      res.status(500).send({ error: err })
+    } catch (error) {
+      res.status(500).send({ error })
     }
-  } 
+  }
 }
--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -1,22 +1,69 @@
 const bcrypt = require('bcrypt')
-const {userService} = require('../user/user.service')
+const { userService } = require('../user/user.service')
+const dbService = require('../../services/db.service')
+const { makeId } = require('../../services/util.service')
 const logger = require('../../services/logger.service')
+const { ObjectId } = require('mongodb')
 
 const saltRounds = 10
+
+var tokens = []
 
 module.exports = {
   authService: {
     login: async ({ email, password }) => {
       logger.debug(`auth.service - login with email: ${email}`)
       if (!email || !password) throw new Error('email and password are required!')
-
       const user = await userService.getByEmail(email)
       if (!user) return Promise.reject('Invalid email or password')
       const match = await bcrypt.compare(password, user.password)
+      delete user.password
       if (!match) return Promise.reject('Invalid email or password')
 
-      delete user.password
-      return user
+      const collection = await dbService.getCollection('token')
+      const newToken = makeId(10)
+      const newHash = await bcrypt.hash(newToken, saltRounds)
+      const {
+        ops: [{ _id: newTokenId }]
+      } = await collection.insertOne({
+        hash: newHash,
+        createdAt: new Date(),
+        userId: ObjectId(user._id).toHexString()
+      })
+      const login = {
+        token: newToken,
+        tokenId: ObjectId(newTokenId).toHexString(),
+        userId: ObjectId(user._id).toHexString()
+      }
+      return { user, login }
+    },
+
+    tokenLogin: async ({ token, tokenId, userId }) => {
+      try {
+        const collection = await dbService.getCollection('token')
+        const { value } = await collection.findOneAndUpdate(
+          { _id: ObjectId(tokenId) },
+          { $set: { createdAt: new Date() } }
+        )
+        console.log(value);
+        const match = await bcrypt.compare(token, value.hash)
+        if (!match) return Promise.reject('Invalid token')
+        const user = await userService.getById(userId)
+        return { user, login: { token, tokenId, userId } }
+      } catch (error) {
+        console.error('ERROR: Could not check token', error)
+        throw new Error(error)
+      }
+    },
+
+    logout: async ({ tokenId }) => {
+      try {
+        const collection = await dbService.getCollection('token')
+        await collection.findOneAndDelete({ _id: ObjectId(tokenId) })
+      } catch (error) {
+        console.error('ERROR: Could not delete login token', error)
+        throw new Error(error)
+      }
     },
 
     signup: async ({ email, password, fullname, imgUrl, boards }) => {
